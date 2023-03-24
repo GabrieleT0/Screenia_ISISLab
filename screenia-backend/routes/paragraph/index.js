f4ebb9c812b2980e516c9b104f34d9ff4d8c7bd9
gerardodonnarumma99 <gerardodonnarumma99@gmail.com>
2023-03-24 11:42:52 +0100

    Fix db paragraph

--- screenia-backend/routes/paragraph/index.js
+++ screenia-backend/routes/paragraph/index.js
@@ -1,44 +1,7 @@
 const express = require('express');
-import initModels from "../../models/init-models";
-import { config } from "../../utils/database/config";
-import { database } from "../../utils/database/sequelizeDB";
+import { paragraph } from "../../models";
 const path = require('path');
 const router = express.Router();
-const knex = require('knex')(config);
-
-/*router.get('/:idOpera/:idBook/:idChapter', async function (req, res) {
-    const idOpera = req.params.idOpera;
-    const idBook = req.params.idBook;
-    const idChapter = req.params.idChapter;
-  
-    if(!idOpera) {
-      return res.send(400, "Param idOpera is required!");
-    }
-
-    if(!idBook) {
-        return res.send(400, "Param idBook is required!");
-    }
-
-    if(!idChapter) {
-        return res.send(400, "Param idChapter is required!");
-    }
-
-    try {
-        const result = await knex()
-        .select('*')
-        .from('paragraph')
-        .where({ 
-            id_opera: idOpera, 
-            number_book: idBook,
-            number_chapter: idChapter 
-        });
-
-        return res.send(result);
-    } catch(e) {
-        return res.status(500).send(e.message);
-    }
-
-})*/
 
 router.get('/:idOpera/:idBook/:idChapter', async function (req, res) {
   const idOpera = req.params.idOpera;
@@ -58,8 +21,7 @@ router.get('/:idOpera/:idBook/:idChapter', async function (req, res) {
   }
 
   try {
-    const Models = await initModels(database);;
-    const Paragraph = Models.paragraph;
+    const Paragraph = paragraph;
     
     const results = await Paragraph.findAll({
       where: {
@@ -76,50 +38,5 @@ router.get('/:idOpera/:idBook/:idChapter', async function (req, res) {
   }
 
 })
-
-/*TO DO: da modificare con sequelize
-router.get('/:idOpera/:idBook/:idChapter/:idParagraph', async function (req, res) {
-  const idOpera = req.params.idOpera;
-  const idBook = req.params.idBook;
-  const idChapter = req.params.idChapter;
-  const idParagraph = req.params.idParagraph;
-
-  if(!idOpera) {
-    return res.send(400, "Param idOpera is required!");
-  }
-
-  if(!idBook) {
-    return res.send(400, "Param idBook is required!");
-  }
-
-  if(!idChapter) {
-    return res.send(400, "Param idChapter is required!");
-  }
-
-  if(!idParagraph) {
-    return res.send(400, "Param idParagraph is required!");
-  }
-
-  try {
-    const result = await knex()
-        .select('*')
-        .from('paragraph')
-        .where({ 
-            id_opera: idOpera, 
-            number_book: idBook,
-            number_chapter: idChapter,
-            number: idParagraph 
-        });
-
-    if(!result) {
-        return res.status(202).send();
-    }
-
-    return res.send(result[0]);
-  } catch(e) {
-    return res.status(500).send(e.message);
-  }
-
-})*/
 
 module.exports = router;
