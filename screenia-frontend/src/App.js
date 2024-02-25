a242cde141ca29bb879b35bc26bf2533eb72a7de
Gabriele <gabrieletuozzo@gmail.com>
2024-02-25 17:59:42 +0000

    added style for the modal

--- screenia-frontend/src/App.js
+++ screenia-frontend/src/App.js
@@ -1,39 +1,32 @@
-import Navbar from './components/Navbar/Navbar';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useEffect } from 'react';
-import FullScreenLoader from './components/FullScreenLoader/FullScreenLoader';
-import useLoader from './customHooks/loaderHooks/useLoader';
-import { BrowserRouter, useLoaderData, useNavigate } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
-import { userAtom } from './state/user/userAtom';
-import { fetchCheckToken } from './api/authApi';
-import useLocalStorage from './customHooks/localStorage/useLocalStorage';
 import ConfirmModal from './components/Dialog/ConfirmDialog';
-import authTokenAtom from './state/authToken/authTokenAtom';
+import FullScreenLoader from './components/FullScreenLoader/FullScreenLoader';
+import Navbar from './components/Navbar/Navbar';
 import useTokenExpirationChecker from './customHooks/authHooks/useTokenExpirationChecker';
+import useLoader from './customHooks/loaderHooks/useLoader';
 
 function App() {
   const { isOpenLoader } = useLoader();
   useTokenExpirationChecker();
-  
+
   return (
     <>
-        <FullScreenLoader isOpenLoader={isOpenLoader} />
-        <ConfirmModal />
-        <ToastContainer
-          position="top-right"
-          theme="colored"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-        <Navbar />
+      <FullScreenLoader isOpenLoader={isOpenLoader} />
+      <ConfirmModal />
+      <ToastContainer
+        position="top-right"
+        theme="colored"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <Navbar />
     </>
   );
 }
--- screenia-frontend/src/index.js
+++ screenia-frontend/src/index.js
@@ -1,27 +1,31 @@
+import { ThemeProvider, createTheme } from '@mui/material';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { RecoilRoot } from "recoil";
-import { themeOptions } from './theme';
-import { ThemeProvider } from '@mui/material';
-import Navbar from './components/Navbar/Navbar';
-import { createTheme } from '@mui/material';
 import { BrowserRouter } from 'react-router-dom';
-import FullScreenLoader from './components/FullScreenLoader/FullScreenLoader';
+import { RecoilRoot } from "recoil";
+import App from './App';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import { themeOptions } from './theme';
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+  integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+  crossorigin="anonymous"
+/>
 
 const theme = createTheme(themeOptions);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter basename="/screenia">
-            <App />
-        </BrowserRouter>
-      </ThemeProvider>
-    </RecoilRoot>
+  <RecoilRoot>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter basename="/screenia">
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </RecoilRoot>
 );
 
 // If you want to start measuring performance in your app, pass a function
